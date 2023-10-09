2cf23b8c091a36487becf55d5a5ce4efaa29f23a
Kert Järva <kert.jarva@tktk.ee>
2023-10-09 19:50:08 +0300

    add week5 code

---
+++ OrOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math results");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Your biology result");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
